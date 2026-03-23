32c3ef527ec2e352b538f8ccddfabcc79d73d198
treeses757-gif <treeses757@gmail.com>
2026-03-23 08:45:57 +0500

    Delete js directory

--- js/textures.js
+++
@@ -1,8 +0,0 @@
-// Загружаем текстуры и экспортируем их
-const tankBlueImg = new Image();
-const tankRedImg = new Image();
-tankBlueImg.src = 'img/tank_blue.png';
-tankRedImg.src = 'img/tank_red.png';
-
-// Можно добавить промисы для ожидания загрузки, но пока просто экспортируем
-export { tankBlueImg, tankRedImg };
